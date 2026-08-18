8618ca1cd66da4e6c547fba76ebf355b0206232e
Brijesh Kumar Saini <Brijesh2025.dev@gmail.com>
2026-08-18 13:18:19 +0530

    Fix Terraform module paths

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "resouce-group" {
-  source    = "../../Module/Resouce_group"
+  source    = "./Module/Resouce_group"
   RGbrijesh = var.RGbrijesh
 }
 module "stoarge_account" {
-  source        = "../../Module/Storage-account"
+  source        = "./Module/Storage-account"
   brieshstoarge = var.brieshstoarge
 }
